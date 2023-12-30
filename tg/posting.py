6b73a3c787eb8906d0d8377b85362c8e09a9ffd5
Denis K <mr.lobzuk@gmail.com>
2023-12-30 04:17:11 +0300

    fixed empty photo/video message

--- tg/posting.py
+++ tg/posting.py
@@ -3,16 +3,19 @@ from main import bot
 
 async def cmd_post(post, CHANNEL_ID):
 
-    text = post.get('text', '')
-    photo_url = None
+    try:
+        text = post.get('text', '')
+        photo_url = None
 
-    if 'attachments' in post:
-        for attachment in post['attachments']:
-            if attachment['type'] == 'photo':
-                photo_sizes = attachment['photo']['sizes']
-                photo_url = photo_sizes[-1]['url']
+        if 'attachments' in post:
+            for attachment in post['attachments']:
+                if attachment['type'] == 'photo':
+                    photo_sizes = attachment['photo']['sizes']
+                    photo_url = photo_sizes[-1]['url']
 
-    if photo_url:
-        await bot.send_photo(chat_id=CHANNEL_ID, photo=photo_url, caption=text)
-    else:
-        await bot.send_message(chat_id=CHANNEL_ID, text=text)
+        if photo_url:
+            await bot.send_photo(chat_id=CHANNEL_ID, photo=photo_url, caption=text)
+        else:
+            await bot.send_message(chat_id=CHANNEL_ID, text=text)
+    except Exception as e:
+        pass
