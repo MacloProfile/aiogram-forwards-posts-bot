3c2c24a085d6a79c0958275b95159d52efde2468
Denis K <mr.lobzuk@gmail.com>
2024-01-02 22:44:09 +0300

    added forwarding of video posts (only for open communities)

--- existing.py
+++ existing.py
@@ -1,23 +1,36 @@
-from main import bot
-from aiogram import types
+import vk_api
+import requests
 
-
-async def is_valid_telegram_channel(id):
+def download_vk_video(video_url, output_path='download/video.mp4'):
     try:
-        chat_info = await bot.get_chat(chat_id=id)
-        return chat_info.type == types.ChatType.CHANNEL
-    except Exception as e:
-        return False
+        # Разбираем URL видео и получаем необходимую информацию
+        video_id = video_url.split('_')[1].split('?')[0]
+        owner_id = video_url.split('-')[1].split('_')[0]
 
+        # Авторизация в VK
+        vk_session = vk_api.VkApi(token='vk1.a.zhhZwm56ortt3aDydy8tHQr_fssb3snptMlgTOL2LBvJpC-1AKQ8n47QEa-rwvy_y5PFlaYc30K-0AEIQrHgXGMC8DD9HMmcSyXXzi_qBAC6vhvnCTT7eGpzxqPhtXS8cz2xJICdKNieLyfV3E3hk8SSXoTHnUlU9CJ0y4rL4_-vwyXwyPlp_pZ05z6PMYlCqssBSRbRwNvBLu0lrxRoRQ')  # Замените 'YOUR_ACCESS_TOKEN' на ваш токен VK
+        vk = vk_session.get_api()
 
-async def is_vk_group_exists(api, group_id):
-    try:
-        group_info = api.groups.getById(group_id=group_id)
-        return True
-    except Exception as e:
-        if e.code == 125:
-            return False
+        # Получаем информацию о видео
+        video_info = vk.video.get(owner_id=owner_id, videos=owner_id + '_' + video_id, extended=1)
+
+        if 'items' in video_info and len(video_info['items']) > 0:
+            # Получаем прямую ссылку на видео
+            direct_url = video_info['items'][0]['player']
+
+            # Скачиваем видео
+            response = requests.get(direct_url)
+            if response.status_code == 200:
+                with open(output_path, 'wb') as video_file:
+                    video_file.write(response.content)
+                print(f"Видео успешно скачано и сохранено по пути: {output_path}")
+            else:
+                print(f"Не удалось скачать видео. Статус код: {response.status_code}")
         else:
-            raise
+            print("Не удалось получить информацию о видео. Проверьте правильность URL.")
+    except Exception as e:
+        print(f"Произошла ошибка при получении информации о видео: {e}")
 
-
+# Пример использования
+video_url = 'https://vk.com/video-223802102_456239075?list=f51cf7450373fd0705'
+download_vk_video(video_url)
--- tg/posting.py
+++ tg/posting.py
@@ -1,11 +1,13 @@
 from main import bot
+import yt_dlp
+import os
 
 
 async def cmd_post(post, CHANNEL_ID):
-
     try:
         text = post.get('text', '')
         photo_url = None
+        video_url = None
 
         if 'attachments' in post:
             for attachment in post['attachments']:
@@ -13,9 +15,38 @@ async def cmd_post(post, CHANNEL_ID):
                     photo_sizes = attachment['photo']['sizes']
                     photo_url = photo_sizes[-1]['url']
 
+                elif attachment['type'] == 'video':
+                    owner_id = post['attachments'][0]['video']['owner_id']
+                    video_id = post['attachments'][0]['video']['id']
+                    video_url = f"https://vk.com/video{owner_id}_{video_id}"
+
         if photo_url:
             await bot.send_photo(chat_id=CHANNEL_ID, photo=photo_url, caption=text)
-        else:
+        elif video_url is not None:
+            await send_video(CHANNEL_ID, video_url, text)
+        elif text:
             await bot.send_message(chat_id=CHANNEL_ID, text=text)
+
     except Exception as e:
-        pass
+        print(f"Ошибка: {e}")
+
+
+async def send_video(channel_id, video_url, text):
+    try:
+        video_path = download_video(video_url)
+        with open(video_path, 'rb') as video_file:
+            await bot.send_video(channel_id, video_file, caption=text)
+        os.remove(video_path)
+    except Exception as e:
+        print(f"Ошибка при отправке видео: {e}")
+
+
+def download_video(video_url):
+    try:
+        ydl_opts = {'outtmpl': 'downloads/%(title)s.%(ext)s', 'progress_hooks': [], 'quiet': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(video_url, download=True)
+            return os.path.join('downloads', f"{info['title']}.{info['ext']}")
+    except:
+        print("Произошла ошибка при скачивании видео")
+        return None
